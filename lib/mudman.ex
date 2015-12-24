c5228285663cc4c2d3e84ee3f97fa8a12bba0d2a
Devon Finninger <devon.finninger@peopleadmin.com>
2015-12-24 12:56:12 -0600

    breaking out database files

--- lib/mudman.ex
+++ lib/mudman.ex
@@ -2,7 +2,16 @@ defmodule Mudman do
   use Application
 
   def start(_type, args) do
-    { :ok, _pid } = Mudman.Supervisor.start_link(args)
+    import Supervisor.Spec, warn: false
+
+    children = [
+      supervisor(Mudman.Repo, [])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Mudman.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 
   def main(_args) do
--- mix.exs
+++ mix.exs
@@ -16,25 +16,24 @@ defmodule Mudman.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [
-      applications: [:logger],
+      applications: [
+        :logger,
+        :postgrex,
+        :ecto
+      ],
       mod: { Mudman, [] }
     ]
   end
 
-  def escript_config do
+  defp escript_config do
     [main_module: Mudman]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:poison, "~> 1.5"},
+      {:postgrex, ">= 0.0.0"},
+      {:ecto, "~> 1.1"}
+    ]
   end
 end
